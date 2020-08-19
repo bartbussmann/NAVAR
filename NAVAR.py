5a4324654055881f5846d197102f4dd2773d1852
bartbussmann <bartbussmann@gmail.com>
2020-08-19 11:02:57 +0200

    adds NAVAR-LSTM

--- NAVAR.py
+++ NAVAR.py
@@ -45,3 +45,53 @@ class NAVAR(nn.Module):
         predictions = torch.sum(contributions, dim=1).squeeze() + self.biases
         contributions = contributions.view([-1, self.num_nodes*self.num_nodes, 1]).squeeze()
         return predictions, contributions
+
+class NAVARLSTM(nn.Module):
+    def __init__(self, num_nodes, num_hidden, maxlags, hidden_layers=1, dropout=0):
+        """
+        Neural Additive Vector AutoRegression (NAVAR) model
+        Args:
+            num_nodes: int
+                The number of time series (N)
+            num_hidden: int
+                Number of hidden units per layer
+            maxlags: int
+                Maximum number of time lags considered (K)
+            hidden_layers: int
+                Number of hidden layers
+            dropout:
+                Dropout probability of units in hidden layers
+        """
+        super(NAVARLSTM, self).__init__()
+
+        self.num_nodes = num_nodes
+        self.num_hidden = num_hidden
+
+        self.lstm_list = nn.ModuleList()
+        self.fc_list = nn.ModuleList()
+        for node in range(self.num_nodes):
+            self.lstm_list.append(nn.LSTM(1, num_hidden, hidden_layers, dropout=dropout, batch_first=True))
+            self.fc_list.append(nn.Linear(num_hidden, num_nodes))
+
+        self.biases = nn.Parameter(torch.ones(1, num_nodes) * 0.0001)
+
+    def forward(self, x):
+        contributions = torch.zeros((x.shape[0], self.num_nodes*self.num_nodes)).cuda()
+
+        # we split the input into the components
+        x = x.split(1, dim=1)
+
+        # then we apply the LSTM layers and calculate the contributions
+        for node in range(self.num_nodes):
+            model_input = torch.transpose(x[node], 1, 2)
+            lstm = self.lstm_list[node]
+            fc = self.fc_list[node]
+            lstm_output, _ = lstm(model_input)
+            contributions[:, node*self.num_nodes:(node+1)*self.num_nodes] = fc(lstm_output[:, -1, :])
+
+        contributions = contributions.view([-1, self.num_nodes, self.num_nodes, 1])
+        predictions = torch.sum(contributions, dim=1).squeeze() + self.biases
+        contributions = contributions.view([-1, self.num_nodes*self.num_nodes, 1]).squeeze()
+        return predictions, contributions
+        
+
